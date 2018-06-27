40a56f1daa973b6ea3c891676e58e11da27ddc2f
Joel DeJesus <joel.dejesus@ad.infosys.com>
2018-06-27 18:22:34 -0400

    adjusted index.js

--- index.js
+++ index.js
@@ -12,33 +12,36 @@ server.use(bodyParser.urlencoded({
 
 server.use(bodyParser.json());
 
-server.post('/get-movie-details', (req, res) => {
+server.post('/poke', (req, res) => {
 
-    const movieToSearch = req.body.queryResult && req.body.queryResult.parameters && req.body.queryResult.parameters.movie ? req.body.queryResult.parameters.movie : 'The Godfather';
-    const reqUrl = encodeURI(`http://www.omdbapi.com/?t=${movieToSearch}&apikey=${API_KEY}`);
+   // const movieToSearch = req.body.queryResult && req.body.queryResult.parameters && req.body.queryResult.parameters.movie ? req.body.queryResult.parameters.movie : 'The Godfather';
+   if(req.body.queryResult.action ==='pokemon.pokemon-custom'){
+    const pokemon= req.body.queryResult.parameters.choice;
+    const reqUrl = encodeURI(`http://pokeapi.co/api/v2/pokemon/${pokemon}`);
     http.get(reqUrl, (responseFromAPI) => {
         let completeResponse = '';
         responseFromAPI.on('data', (chunk) => {
             completeResponse += chunk;
         });
         responseFromAPI.on('end', () => {
-            const movie = JSON.parse(completeResponse);
-            let dataToSend = movieToSearch === 'The Godfather' ? `I don't have the required info on that. Here's some info on 'The Godfather' instead.\n` : '';
-            dataToSend += `${movie.Title} is a ${movie.Actors} starer ${movie.Genre} movie, released in ${movie.Year}. It was directed by ${movie.Director}`;
+            const pokemonChoice = JSON.parse(completeResponse);
+            let dataToSend = pokemon === 'Pikachu' ? `I don't have the required info on that. Here's some info on Pikachu instead.\n` : '';
+            dataToSend += `${poke.name} is a ${poke.types.type} pokemon.`;
 
             return res.json({
                 speech: dataToSend,
                 displayText: dataToSend,
-                source: 'get-movie-details'
+                source: 'poke'
             });
         });
     }, (error) => {
         return res.json({
             speech: 'Something went wrong!',
             displayText: 'Something went wrong!',
-            source: 'get-movie-details'
+            source: 'poke'
         });
     });
+   }
 });
 
 server.listen((process.env.PORT || 8000), () => {
