339c2a75405e6303811f2cedbfaef73be454aa37
Joel DeJesus <joel.dejesus@ad.infosys.com>
2018-06-27 18:29:42 -0400

    adjusted poke

--- index.js
+++ index.js
@@ -16,8 +16,9 @@ server.post('/poke', (req, res) => {
 
    // const movieToSearch = req.body.queryResult && req.body.queryResult.parameters && req.body.queryResult.parameters.movie ? req.body.queryResult.parameters.movie : 'The Godfather';
    if(req.body.queryResult.action ==='pokemon.pokemon-custom'){
-    const pokemon= req.body.queryResult.parameters.choice;
-    const reqUrl = encodeURI(`http://pokeapi.co/api/v2/pokemon/${pokemon}`);
+    const Pokemon= req.body.queryResult.parameters.choice.pokemon;
+    console.log(`${pokemon}`)
+    const reqUrl = encodeURI(`http://pokeapi.co/api/v2/pokemon/${Pokemon}`);
     http.get(reqUrl, (responseFromAPI) => {
         let completeResponse = '';
         responseFromAPI.on('data', (chunk) => {
@@ -25,7 +26,7 @@ server.post('/poke', (req, res) => {
         });
         responseFromAPI.on('end', () => {
             const pokemonChoice = JSON.parse(completeResponse);
-            let dataToSend = pokemon === 'Pikachu' ? `I don't have the required info on that. Here's some info on Pikachu instead.\n` : '';
+            let dataToSend = Pokemon === 'Pikachu' ? `I don't have the required info on that. Here's some info on Pikachu instead.\n` : '';
             dataToSend += `${poke.name} is a ${poke.types.type} pokemon.`;
 
             return res.json({
