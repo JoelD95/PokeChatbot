34b9222fe3ae30a9e696253f64f964c29eacd524
Joel DeJesus <joel.dejesus@ad.infosys.com>
2018-06-28 11:54:17 -0400

    pokemon works!

--- index.js
+++ index.js
@@ -10,17 +10,24 @@ const server = express();
 server.use(bodyParser.json());
 server.post('/poke',(req,res)=>{
     let link='';
-    if(req.body.queryResult.parameters.choice.id === 'undefined'){
-        link=req.body.queryResult.parameters.choice.pokemon
+    if(req.body.queryResult.parameters.choice.id !== 'undefined'){
+        link=req.body.queryResult.parameters.choice.id;
     }
-    else if (req.body.queryResult.parameters.choice.pokemon=== 'undefined')
-    link=req.body.queryResult.parameters.choice.id
-    else if (req.body.queryResult.parameters.choice.id === 'undefined' && req.body.queryResult.parameters.choice.pokemon === 'undefined')
-    link=req.body.queryResult.parameters.choice
+   else if (req.body.queryResult.parameters.choice.pokemon !== 'undefined'){
+        link=req.body.queryResult.parameters.choice.pokemon;
+    }
+    if (req.body.queryResult.parameters.choice.id === 'undefined' && req.body.queryResult.parameters.choice.pokemon === 'undefined'){
+        link=req.body.queryResult.parameters.choice
+    };
 request(`${hostname}${path}${link}/`,(err,resp,body)=> {
     let test = '';
+    let type2 = '';
     const poke = JSON.parse(body);
-    test = `${poke.name} is a ${poke.types[1].type.name}, ${poke.types[0].type.name} pokemon!`;
+    if((poke.types).length === 1){
+        console.log((poke.types).length)
+        test = `${poke.name} is a ${poke.types[0].type.name} pokemon!`;
+    }
+    else {test = `${poke.name} is a ${poke.types[1].type.name}, ${poke.types[0].type.name} pokemon!`;}
     request(`${hostname}ability/${poke.abilities[1].ability.name}`,(err,resp,body)=> {
         const abl= JSON.parse(body);
         let ability= abl.effect_entries[0].short_effect;
